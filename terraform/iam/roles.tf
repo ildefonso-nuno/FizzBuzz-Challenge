d80eaad3e5858c71c3c56b13725f8ac84c62ab1f
Nuno Ildefonso <ildefonso.nuno@gmail.com>
2024-02-12 16:10:36 +0100

    Update to terraform IAM

--- terraform/iam/roles.tf
+++ terraform/iam/roles.tf
@@ -1,4 +1,60 @@
-resource "google_project_iam_binding" "service_usage_viewer" {
+resource "google_project_iam_binding" "role-admin" {
+project = var.project_id
+role    = "roles/iam.roleAdmin"
+
+members = [
+"serviceAccount:${var.service_account_email}",
+]
+}
+
+resource "google_project_iam_binding" "service-account-user" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+
+  members = [
+    "serviceAccount:${var.service_account_email}",
+  ]
+  depends_on = [google_project_iam_binding.role-admin]
+}
+
+
+resource "google_project_iam_custom_role" "beluga-role" {
+  role_id     = "belugaRole"
+  title       = "Beluga Service Role"
+  description = "Custom role for the Beluga Service"
+  permissions = [
+    "compute.firewalls.create",
+    "compute.firewalls.get",
+    "compute.firewalls.delete",
+    "compute.networks.create",
+    "compute.networks.get",
+    "compute.networks.delete",
+    "compute.networks.updatePolicy",
+    "compute.subnetworks.create",
+    "compute.subnetworks.get",
+    "compute.subnetworks.delete",
+    "run.services.create",
+    "run.services.get",
+    "run.services.update",
+    "run.services.delete",
+    "run.services.setIamPolicy",
+    "run.services.getIamPolicy",
+    "artifactregistry.repositories.uploadArtifacts"
+  ]
+  depends_on = [google_project_iam_binding.service-account-user]
+}
+
+resource "google_project_iam_binding" "beluga-role-binding" {
+  project = var.project_id
+  role    = google_project_iam_custom_role.beluga-role.id
+
+  members = [
+    "serviceAccount:${var.service_account_email}",
+  ]
+  depends_on = [google_project_iam_custom_role.beluga-role]
+}
+
+/*resource "google_project_iam_binding" "service_usage_viewer" {
   project = var.project_id
   role    = "roles/serviceusage.serviceUsageViewer"
 
@@ -18,5 +74,5 @@ resource "google_project_iam_member" "service_account_pubsub_subscriber" {
   member  = "serviceAccount:${var.service_account_email}"
 
   depends_on = [google_project_iam_binding.service_usage_viewer]
-}
+}*/
 
